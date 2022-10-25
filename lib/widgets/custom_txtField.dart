d9ecc96f2bcfa9d3fd637db50625e0bc1a248b59
Estudiante Bloque 09 Sala 511  B09S511est <B09S511est@AULASMED.UPB>
2022-10-25 13:31:16 -0500

    solucionado textfield, falta contraseña

--- lib/widgets/custom_txtField.dart
+++ lib/widgets/custom_txtField.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class CustomTextField extends StatelessWidget {
+class CustomTextField extends StatefulWidget {
   CustomTextField({
     Key? key,
     required this.obs, //OPCIONAL
@@ -10,30 +10,46 @@ class CustomTextField extends StatelessWidget {
     required this.color, 
   }) : super(key: key);
 
-  final bool? obs; //puede ser nulla
+  final bool obs; //puede ser nulla
   final String text;
   final Color? color; 
   final double x, y;
 
   @override
+  State<CustomTextField> createState() => _CustomTextFieldState();
+}
+
+class _CustomTextFieldState extends State<CustomTextField> {
+  @override
   Widget build(BuildContext context) {
-    return TextField(
-      obscureText: obs!,
-      decoration: InputDecoration(
-        //hintText: text,
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular((50))),
-          borderSide: BorderSide(color: Colors.black, width: 0.1),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal:20),
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10)
         ),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular((50))),
-          borderSide:
-              BorderSide(color: Color.fromARGB(255, 235, 165, 3), width: 2),
-        ),
-        iconColor: color,
-        labelText: text,
-        labelStyle: TextStyle(
-          color: Color.fromARGB(92, 61, 61, 61),
+        child: TextField(
+          obscureText: widget.obs,
+          
+          
+          decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular((10))),
+              borderSide: BorderSide(color: Colors.black, width: 0.1),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular((10))),
+              borderSide:
+                  BorderSide(color: Color.fromARGB(255, 235, 165, 3), width: 2),
+            ),
+            iconColor: widget.color,
+            labelText: widget.text,
+            labelStyle: TextStyle(
+              color: Color.fromARGB(92, 61, 61, 61),
+            ),
+            
+          ),
         ),
       ),
     );
