36ce3534e4eca9966d122876b0c5492bb3047f8a
SamuelSanta <samuelsanta2004@gmail.com>
2022-10-25 03:29:15 -0500

    texto ingresar, funcion de txt, fonts

--- lib/vistas/SignIn.dart
+++ lib/vistas/SignIn.dart
@@ -10,7 +10,7 @@ class SignIn extends StatelessWidget {
     double screenwidth = MediaQuery.of(context).size.width;
     double screenheight = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Center(
+        body: Container(
       child: Container(
         width: screenwidth,
         height: screenheight,
@@ -18,17 +18,28 @@ class SignIn extends StatelessWidget {
           color: Color.fromARGB(255, 75, 109, 184),
           borderRadius: BorderRadius.circular(25),
         ),
-        child: Container(
-          width: 1,
-          height: 1,
-          alignment: Alignment.bottomLeft,
-          child: Image.asset(
-            "assets/platofondo.png",
-            //width: screenwidth * 0.55,
-            height: screenheight * 0.45,
-            scale: 0.1,
+        child: Column(children: [
+          SizedBox(
+            height: (screenheight * 0.08),
           ),
-        ),
+          Spacer(flex: 1),
+          Container(
+            decoration: BoxDecoration(),
+            child: txt("Ingresar", Colors.white, 12, "Poppins"),
+            alignment: Alignment(-0.7, -0.1), //no sirve el height
+          ),
+          Container(
+            width: screenwidth,
+            height: screenheight * 0.875,
+            alignment: Alignment.bottomLeft,
+            child: Image.asset(
+              fit: BoxFit.cover,
+              "assets/imgs/platofondo.png",
+              height: screenheight * 0.45,
+              scale: 0.1,
+            ),
+          )
+        ]),
       ),
     ));
   }
--- lib/widgets/funciones.dart
+++ lib/widgets/funciones.dart
@@ -1,1 +1,25 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+
+Text txt(String text, Color colr, double tam, String estilo) {
+  return Text(text,
+      style: TextStyle(
+          fontSize: tam,
+          fontWeight: FontWeight.w500,
+          color: colr,
+          fontFamily: estilo),
+      textAlign: TextAlign.right);
+}
+
+/*
+Text txt(String text, Color colr, double tam, String estilo) {
+  return Text(text,
+      style: TextStyle(
+          fontSize: tam,
+          fontWeight: FontWeight.w500,
+          color: colr,
+          fontFamily: estilo),
+      textAlign: TextAlign.center);
+}
+*/
