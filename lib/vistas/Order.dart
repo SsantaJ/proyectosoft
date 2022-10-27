46be0bc73c2dad421d71ea245e749e01260f5a5c
Mateo Gonzalez <mateogt0903@gmail.com>
2022-10-26 23:04:18 -0500

    boton y titulo vista de order, agregados

--- lib/vistas/Order.dart
+++ lib/vistas/Order.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:proyectosoft/widgets/funciones.dart';
 
 class Order extends StatelessWidget {
   const Order({Key? key}) : super(key: key);
@@ -10,7 +11,31 @@ class Order extends StatelessWidget {
     double screenwidth = MediaQuery.of(context).size.width;
     double screenheight = MediaQuery.of(context).size.height;
     return Scaffold(
-      body: Container
-    );
+        body: Center(
+      child: Container(
+          width: screenwidth,
+          height: screenheight,
+          decoration: const BoxDecoration(
+            color: Color.fromARGB(255, 75, 109, 184),
+          ),
+          child: Column(
+            children: [
+              SizedBox(
+                height: (screenheight * 0.06),
+              ),
+              Row(children: [
+                SizedBox(
+                  width: screenwidth*0.08,
+                ),
+                boton(() {}, Color.fromARGB(0, 255, 255, 255), "<",
+                    Colors.white, screenwidth * 0.11, screenheight * 0.055, 30),
+                SizedBox(
+                  width: screenwidth*0.1,
+                ),
+                txt("Carrito de comida", Colors.white, 20, "Poppins")
+              ]),
+            ],
+          )),
+    ));
   }
 }
